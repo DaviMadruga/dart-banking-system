69386b096699876da82b95992548080de85e1a3a
Davi Madruga <dcavalcanti14@gmail.com>
2026-04-20 18:15:51 -0300

    feat: implementa menu interativo com listagem, busca e transferência de contas

--- bin/anybank.dart
+++ bin/anybank.dart
@@ -6,78 +6,95 @@ import 'package:anybank/models/conta_empresa.dart';
 import 'package:anybank/models/conta_investimento.dart';
 import 'package:anybank/services/banco.dart';
 import 'package:anybank/utils/exibicao.dart';
+import 'dart:io';
 
 void main(){
+  Banco banco = criarBancoInicial();
+  bool executando = true;
+  String? opcao = "";
+
+  while(executando){
+    print("");
+    mostrarMenu();
+    print("Escolha uma opção:");
+    opcao = stdin.readLineSync();
+    
+    switch(opcao){
+      case "1":
+        mostrarSecao("LISTAGEM DE CONTAS");
+        banco.listarContas();
+      break;
+      case "2":
+        mostrarSecao("BUSCA DE CONTA POR TITULAR");
+        String titular = lerEntrada("Digite o nome do titular:");
+        Conta? contaEncontrada = banco.buscarContaPorTitular(titular);
+
+        if(contaEncontrada != null){
+          print("Conta encontrada: ${contaEncontrada.titular} | Saldo: R\$${contaEncontrada.saldo}");
+        }else{
+          print("Conta não encontrada");
+        }
+      break;
+      case "3":
+        mostrarSecao("TRANSFERENCIA DE CONTAS");
+        String origem = lerEntrada("Digite o nome do titular da conta de origem:");
+        String destino = lerEntrada("Digite o nome do titular da conta de destino:");
+        String valorTexto = lerEntrada("Digite o valor da transferência:");
+
+        double? valor = double.tryParse(valorTexto);
+
+        if(valor == null){
+          print("Valor inválido!");
+          break;
+        }
+
+        bool resultado = banco.transferir(origem, destino, valor);
+
+        mostrarResultado(
+          resultado, 
+          "Transferência realizada com sucesso", 
+          "Não foi possível realizar a transferência"
+        );
+        
+        Conta? contaOrigem = banco.buscarContaPorTitular(origem);
+        Conta? contaDestino = banco.buscarContaPorTitular(destino);
+
+        if(contaOrigem != null){
+          mostrarSaldo(contaOrigem);
+        }
+        if(contaDestino != null){
+          mostrarSaldo(contaDestino);
+        }
+      break;
+      case "4":
+        print("Encerrando o sistema...");
+        executando = false;
+      break;
+      default:
+        print("Opção inválida. Tente novamente");
+    }
+  }
+}
+
+Banco criarBancoInicial(){
+  Banco banco = Banco("AnyBank");
+
   ContaCorrente contaChris = ContaCorrente("Chris", 4000);
   ContaPoupanca contaDenize = ContaPoupanca("Denize", 4000);
-  ContaSalario contaCatarina = ContaSalario("Catarina", 0, "111111111", "AnyBank");
+  ContaSalario contaCatarina = ContaSalario("Catarina", 4000, "111111111", "AnyBank");
   ContaEmpresa contaMatheus = ContaEmpresa("Matheus", 2000);
-  ContaInvestimento contaRoberta = ContaInvestimento("Roberta", 2000);
+  ContaInvestimento contaRoberta = ContaInvestimento("Roberta", 3030); 
 
-  mostrarSecao("ENVIO DE VALOR - CONTA CHRIS");
-  mostrarSaldo(contaChris);
-  bool resultadoEnvioChris = contaChris.enviar(4300);
-  mostrarResultado(resultadoEnvioChris,"Envio realizado com sucesso", "Não foi possível realizar o envio",);
-  mostrarSaldo(contaChris);
-
-  mostrarSecao("ENVIO DE VALOR - CONTA DENIZE");
-  mostrarSaldo(contaDenize);
-  bool resultadoEnvioDenize = contaDenize.enviar(4300);
-  mostrarResultado(resultadoEnvioDenize,"Envio realizado com sucesso", "Não foi possível realizar o envio",);
-  mostrarSaldo(contaDenize);
-
-  mostrarSecao("DEPOSITO DE SALARIO - CONTA CATARINA");
-  mostrarSaldo(contaCatarina);
-  bool resultadoSalarioDepositadoCatarina = contaCatarina.depositarSalario(2500);
-  mostrarResultado(resultadoSalarioDepositadoCatarina,"Salário depositado com sucesso", "Não foi possível depositar o salário",);
-  mostrarSaldo(contaCatarina);
-
-  mostrarSecao("ENVIO DE VALOR - CONTA MATHEUS");
-  mostrarSaldo(contaMatheus);
-  bool resultadoEnvioMatheus = contaMatheus.enviar(1000);
-  mostrarResultado(resultadoEnvioMatheus, "Envio realizado com sucesso", "Não foi possível realizar o envio");
-  mostrarSaldo(contaMatheus);
-
-  mostrarSecao("RECEBIMENTO DE VALOR - CONTA ROBERTA");
-  mostrarSaldo(contaRoberta);
-  bool resultadoRecebimentoRoberta = contaRoberta.receber(1000);
-  mostrarResultado(resultadoRecebimentoRoberta, "Recebimento realizado com sucesso", "Não foi possível realizar o recebimento");
-
-  mostrarSecao("TESTE DE TRANSFERÊNCIA");
-  mostrarSaldo(contaChris);
-  mostrarSaldo(contaDenize);
-  bool resultadoTransferenciaRealizada = contaChris.transferir(contaDenize, 200);
-  mostrarResultado(resultadoTransferenciaRealizada, "Transferência realizada com sucesso", "Não foi possível realizar a transferência");
-  mostrarSaldo(contaChris);
-  mostrarSaldo(contaDenize);
-
-  Banco banco = Banco("AnyBank");
   banco.adicionarConta(contaChris);
   banco.adicionarConta(contaDenize);
   banco.adicionarConta(contaCatarina);
   banco.adicionarConta(contaMatheus);
   banco.adicionarConta(contaRoberta);
 
-  mostrarSecao("CONTAS CADASTRADAS");
-  banco.listarContas();
+  return banco;
+}
 
-  mostrarSecao("BUSCAR CONTA");
-  Conta? contaEncontrada = banco.buscarContaPorTitular("Denize");
-  if(contaEncontrada != null){
-    print("Conta encontrada: ${contaEncontrada.titular} | Saldo: R\$${contaEncontrada.saldo}");
-  }else{
-    print("Conta não encontrada");
-  }
-
-  mostrarSecao("TRANSFERÊNCIA VIA BANCO");
-  mostrarSaldo(contaCatarina);
-  mostrarSaldo(contaDenize);
-  bool resultado = banco.transferir("Catarina", "Denize", 300);
-  if(resultado){
-    print("Transferência realizada com sucesso");
-  }else{
-    print("Falha na transferência");
-  }
-  mostrarSaldo(contaCatarina);
-  mostrarSaldo(contaDenize);
+String lerEntrada(String mensagem){
+  stdout.write(mensagem);
+  return stdin.readLineSync() ?? "";
 }
--- lib/utils/exibicao.dart
+++ lib/utils/exibicao.dart
@@ -15,3 +15,11 @@ void mostrarResultado(bool sucesso,String msgSucesso,String msgErro){
 void mostrarSecao(String titulo){
   print("\n=== $titulo ===");
 }
+
+void mostrarMenu(){
+  mostrarSecao("MENU PRINCIPAL");
+  print("1 - Listar contas");
+  print("2 - Buscar conta por titular");
+  print("3 - Tranferir entre contas");
+  print("4 - Sair");
+}
